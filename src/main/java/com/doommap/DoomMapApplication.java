04ef6816e4f79b2e0bad0bc052ccb8e20c7c8e6f
Ruan Herculano <ruan.pereira@edulabzz.com.br>
2020-02-24 13:08:31 -0300

    Sobe estrutura inicial do projeto com o java 13

---
+++ src/main/java/com/doommap/DoomMapApplication.java
@@ -0,0 +1,13 @@
+package com.doommap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoomMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoomMapApplication.class, args);
+	}
+
+}

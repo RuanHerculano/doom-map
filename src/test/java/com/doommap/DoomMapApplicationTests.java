82e002fe58d1e8905eb0157cdff67de8bd589706
Ruan Herculano <ruan.pereira@edulabzz.com.br>
2020-02-24 19:41:48 -0300

    Faz hello world usando jsp

--- src/test/java/com/doommap/DoomMapApplicationTests.java
+++ src/test/java/com/doommap/DoomMapApplicationTests.java
@@ -1,13 +1,41 @@
 package com.doommap;
 
+import com.doommap.config.SpringConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
-class DoomMapApplicationTests {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-	@Test
-	void contextLoads() {
-	}
+@SpringJUnitWebConfig(SpringConfig.class)
+public class DoomMapApplicationTests {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
+
+    @Test
+    public void testWelcome() throws Exception {
+
+        this.mockMvc.perform(
+                get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(forwardedUrl("/WEB-INF/views/index.jsp"))
+                .andExpect(model().attribute("name", "Hello World!"));
+    }
 
 }
